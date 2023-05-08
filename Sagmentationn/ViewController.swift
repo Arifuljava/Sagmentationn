4dcc91f61062edf9aac2fbe6c6b57d74d0e997b6
Ariful0007 <arifulpub143@gmail.com>
2023-05-08 17:00:34 +0600

    Initial Commit

---
+++ Sagmentationn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sagmentationn
+//
+//  Created by sang on 17/10/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
